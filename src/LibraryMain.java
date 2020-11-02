fadb5e1874302b79d2c3e0440951c4a588415df3
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2020-11-02 12:14:53 +0100

    add

--- src/LibraryMain.java
+++ src/LibraryMain.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -7,9 +10,33 @@ public class LibraryMain {
 		
 		// lägg in arrayList 
 	
-	ArrayList <String> products = new ArrayList <String>(); // Måste lägga till produkter innan jag kan använda Arraylist
+	ArrayList <Movies> movies = new ArrayList<Movies>();
+	ArrayList <Books> books = new ArrayList <Books>();
 	
 	// arraylist for books and movies and arraylist for ALL Products
+	
+	// Serialize data
+	public void saveData() {
+		// the massive object to store all our objects!
+		ArrayList <Object> data = new ArrayList<Object>();
+		/*data.add(myName);
+		data.add(myAge);
+		data.add(myHobbies);
+		data.add(myFriends);*/
+		
+		// the actuall serilazation 
+		
+		try {
+			FileOutputStream fileOut = new FileOutputStream("data.ser");
+			ObjectOutputStream out = new ObjectOutputStream (fileOut);
+			out.writeObject(data);
+			out.close();
+			fileOut.close();
+			System.out.println("Serialized data is saved in data.ser");
+		}catch (IOException i) {
+			i.printStackTrace();
+		}
+		}
 	
 	//LinkedList library = new LinkedList(); 
 	
