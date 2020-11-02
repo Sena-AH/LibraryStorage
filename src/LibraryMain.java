0a68f642be20116c9189199e61b21210b988e09e
Sena A.H <sena@Senas-Air.lan>
2020-11-02 15:35:17 +0100

    add list method in manage class

--- src/LibraryMain.java
+++ src/LibraryMain.java
@@ -9,18 +9,25 @@ import java.util.LinkedList;
 public class LibraryMain {
 
 		
-		// l�gg in arrayList 
-	
+	/*
 	protected ArrayList<Product> allProducts() {
-	ArrayList <Product> products = new ArrayList <Product>(); // M�ste l�gga till produkter innan jag kan anv�nda Arraylist
+	ArrayList <Product> products = new ArrayList <Product>();
 
-	Product harrypotter = new Book(1234, "Harry Potter and the philosophers stone", 200, 300, "J.K Rowling");
-	
+	Product harrypotter = new Book(1234, "Harry Potter and the Philosopher's stone", 179, 223, "J.K Rowling");
+	Product attackontitan = new Movie(9001, "Attack on Titan", 150, 120, 8);
+	Product hitchhiker = new Book(1235, "The Hitchhiker's Guide to the Galaxy", 100, 208, "Douglas Adams");
+	Product spiritedaway = new Movie(9002, "Spirited Away", 80, 125, 8);
 	
 	products.add(harrypotter);
+	products.add(attackontitan);
+	products.add(hitchhiker);
+	products.add(spiritedaway);
+	
+	
 	return products;
 	
 	}
+	*/
 	// Serialize data
 	public void saveData() {
 		// the massive object to store all our objects!
